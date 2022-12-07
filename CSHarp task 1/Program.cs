49075d9c40a56d5f25ec506edc18e8f43e336ed3
unknown <jeyhunny@code.edu.az>
2022-12-08 02:23:52 +0400

    Csharp

---
+++ CSHarp task 1/Program.cs
@@ -0,0 +1,24 @@
+﻿//See https://aka.ms/new-console-template for more information
+// Verilmis n ededinin 3-e ve 7-e bolunub bolunmemesini tapin
+//int n = 42;
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+//    Console.WriteLine("bolunur");
+//}
+//else
+//    Console.WriteLine("bolunmur");
+
+
+//task2 -n ve m edeleri verilir. eger n ve m ededleriher ikisi cutdurse n ve m ededlerinin cemini tapin
+//int n = 10, m = 14, sum = 0;
+//if (n % 2 == 0 && m % 2 == 0)
+//{
+//    sum = n + m;
+//    Console.WriteLine(sum);
+//}
+
+//else
+//{
+//    Console.WriteLine("yanlisdir");
+//}
+
